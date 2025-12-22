1e9b0031a1694434415931a7916cda285371a9ec
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2025-12-22 14:58:30 -0500

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 class Animal:
 
-    def __init__(self, name, appetite, is_hungry = True) -> None:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
 
-        def print_name (self):
+        def print_name(self) -> None:
             print(f"Hello, I'm {self.name}")
 
         def feed(self):
@@ -18,14 +18,14 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name, is_hungry = True) -> None:
+    def __init__(self, name: str, is_hungry = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self):
         print(f"The hunt began!")
 
 class Dog(Animal):
-    def __init__(self, name, is_hungry=True) -> None:
+    def __init__(self, name: str, is_hungry=True) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     def bring_slippers(self):
